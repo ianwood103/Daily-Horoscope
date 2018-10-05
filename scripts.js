e6dc699adfcfb2d3d95ac58e2f4f1db953cba3f5
Ian Wood <30994047+ianwood103@users.noreply.github.com>
2018-10-04 23:30:54 -0400

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,4 +1,5 @@
-
+var horoscope = [];
+var buttonCounter;
 var aquarius= 'img/aquarius.png';
 var aries = 'img/aries.png';
 var capricorn = 'img/capricornus.png';
@@ -28,19 +29,34 @@ var zArray = [
 ];
 
 var wordArray = [
-  'aquarius',
-  'aries',
-  'capricorn',
-  'cancer',
-  'gemini',
-  'leo',
-  'libra',
-  'pisces',
-  'sagittarius',
-  'scorpio',
-  'taurus',
-  'virgo'
+  'Aquarius',
+  'Aries',
+  'Capricorn',
+  'Cancer',
+  'Gemini',
+  'Leo',
+  'Libra',
+  'Pisces',
+  'Sagittarius',
+  'Scorpio',
+  'Taurus',
+  'Virgo'
 ];
+
+var horoscopeArray = {
+  "Aries": 0,
+  "Taurus": 1,
+  "Gemini": 2,
+  "Cancer": 3,
+  "Leo": 4,
+  "Virgo": 5,
+  "Libra": 6,
+  "Scorpio": 7,
+  "Sagittarius": 8,
+  "Capricorn": 9,
+  "Aquarius": 10,
+  "Pisces": 11
+}
 
 var zodiac = 0;
 var zodiacLeft = 11;
@@ -94,11 +110,33 @@ function rightZodiac() {
 }
 
 function horoscopeClick() {
-  alert(zArray[zodiacLeft]);
-  alert(zArray[zodiac]);
-  alert(zArray[zodiacRight]);
+  buttonCounter += 1;
+  console.log(zodiac);
+  if(buttonCounter % 2 == 1) {
+    $('.zodiac-wrapper').fadeOut(100);
+    document.getElementById('horoscope').innerHTML = horoscope[horoscopeArray[wordArray[zodiac]]]["Horoscope"];
+    document.getElementById('horoscope-header').innerHTML = horoscope[horoscopeArray[wordArray[zodiac]]]["Zodiac Sign"];
+    $('#submit-button').attr('value', 'Return');
+    $('.horoscope-message').fadeIn(500);
+    $('.button-wrapper input').addClass('new-color');
+    console.log(horoscope[horoscopeArray[wordArray[zodiac]]]["Zodiac Sign"]);
+  } else {
+    $('.button-wrapper input').removeClass('new-color');
+    $('.zodiac-wrapper').fadeIn(200);
+    $('#submit-button').attr('value', 'Submit');
+    $('.horoscope-message').hide();
+    zodiacSwitch();
+  }
 }
 
 $(document).ready(function() {
   zodiacSwitch();
+  buttonCounter = 0;
+  $('.horoscope-message').hide();
+  $('#horoscope-header', '#horoscope').hide();
+
+  $.getJSON("https://s3.amazonaws.com/scrapy.seattle/Folder1/data.json", function(json) {
+    horoscope = json;
+    console.log(horoscope);
+  });
 });
